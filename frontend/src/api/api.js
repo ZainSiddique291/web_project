71104345e2375a7a42aa4b4be57e6140147e560b
Zain Siddique <zainsiddique291@gmail.com>
2026-05-29 01:02:55 +0500

    connection

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL ;
+// const API_BASE = import.meta.env.VITE_API_URL ;
 // || 'http://localhost:5000/api'
+const API_BASE="https://web-project-4u08.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE,
